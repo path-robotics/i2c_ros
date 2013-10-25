ce76d47ec57c075d74fb1b6c4bdac5141d5eaa42
Bruno Gouveia <bgouveia@gmail.com>
2013-10-25 16:45:43 +0100

    parameters struct

--- src/i2cabstract.h
+++ src/i2cabstract.h
@@ -1,14 +1,32 @@
 #ifndef I2CABSTRACT_H
 #define I2CABSTRACT_H
 
+
+typedef struct _parameters{
+    bool fast;
+
+    //libmpsse specific
+    int vid;
+    int pid;
+    int interface;
+    char * description;
+    char * serial;
+    int index;
+
+    //linux I2CDEV specific
+    char * devicename;
+
+}i2c_paramaters;
+
+
 class I2Cabstract
 {
 public:
-    virtual void open()=0;
+    virtual void open(i2c_paramaters& parameters)=0;
     virtual void close()=0;
 
-    virtual void read()=0;
-    virtual void write()=0;
+    virtual void read(unsigned char address, unsigned char* bytes, int numBytes)=0;
+    virtual void write(unsigned char address, unsigned char* bytes, int numBytes)=0;
 };
 
 #endif // I2CABSTRACT_H
--- src/i2cmpsse.h
+++ src/i2cmpsse.h
@@ -3,10 +3,16 @@
 
 #include "i2cabstract.h"
 
+struct mpsse_context;
+
+
 class I2Cmpsse : public I2Cabstract
 {
 public:
     I2Cmpsse();
+protected:
+    struct mpsse_context * i2c;
+
 };
 
 #endif // I2CMPSSE_H
